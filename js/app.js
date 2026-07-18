79243a1e6f1b956589014a5cbf1c5f4947ac373d
Eliatan Gabriel <eliatangabriel221@gmail.com>
2026-07-18 12:42:13 -0300

    começando o js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const valorForm = document.querySelector("#taskflowform");
+const input = document.querySelector("#taskflow");
+
+valorForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
